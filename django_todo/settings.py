a1e700e2b9e66d8d91861ca366388f5e1011d1e1
Tiyko <johnny86shepherd@gmail.com>
2022-08-31 00:02:41 +0000

    Add host

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '.123.'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-app-todo-list.herokuapp.com']
 
 
 # Application definition
