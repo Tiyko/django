f48def5a0220d74d5edf28846eac302e54ab4419
Tiyko <johnny86shepherd@gmail.com>
2022-09-02 00:22:34 +0000

    change env

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -13,9 +13,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 import dj_database_url
+import env # noqa
 
-development = os.environ.get('DEVELOPMENT', False)
-
+development = os.environ.get('DEV_MODE', False)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
